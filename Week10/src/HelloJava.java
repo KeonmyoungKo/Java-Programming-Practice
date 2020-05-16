7f94ee3aeb6810597e1cf802140ed187c85e1e94
kkm99 <kkm99@DESKTOP-GTKCTEF>
2020-05-16 21:54:28 +0900

    HelloJava

---
+++ Week10/src/HelloJava.java
@@ -0,0 +1,11 @@
+//Week10_2019312942 °í°Ç¸í
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!");
+		
+	}
+
+}
